c8bd01d70b86dbf70b231a07b6508e946ecd4f87
Jose Sifuentes <jsifuen711@gmail.com>
2022-04-25 19:19:56 -0700

    New Update
    added stacked card for main slider with static details on the food being displayed.

--- app_1/lib/home/food.page.body.dart
+++ app_1/lib/home/food.page.body.dart
@@ -1,4 +1,9 @@
+import 'package:app_1/utils/colors.dart';
+import 'package:app_1/widgets/icon.and.text.widget.dart';
+import 'package:app_1/widgets/small.text.dart';
 import 'package:flutter/material.dart';
+
+import '../widgets/big.text.dart';
 
 class FoodPageBody extends StatefulWidget {
   const FoodPageBody({Key? key}) : super(key: key);
@@ -8,11 +13,27 @@ class FoodPageBody extends StatefulWidget {
 }
 
 class _FoodPageBodyState extends State<FoodPageBody> {
-  PageController pageController = PageController(viewportFraction: 0.9);
+  PageController pageController = PageController(viewportFraction: 0.85);
+  var _currPageValue = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController.addListener(() {
+      setState(() {
+        _currPageValue = pageController.page!;
+        //bottom line of code is to check scroller/slider is working. remove forward slashes to
+        //test in debug console
+        //print("Current value is " + _currPageValue.toString());
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.redAccent,
+      //remove bottom line of code that is commentd out (//) before deployment along with this comment
+      //color: Colors.redAccent,
       height: 320,
       child: PageView.builder(
           controller: pageController,
@@ -28,7 +49,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
       children: [
         Container(
           height: 220,
-          margin: const EdgeInsets.only(left: 5, right: 5),
+          margin: const EdgeInsets.only(left: 10, right: 10),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(30),
             color: index.isEven
@@ -43,12 +64,60 @@ class _FoodPageBodyState extends State<FoodPageBody> {
         Align(
           alignment: Alignment.bottomCenter,
           child: Container(
-            height: 140,
-            margin: const EdgeInsets.only(left: 40, right: 40, bottom: 15),
+            height: 130,
+            margin: const EdgeInsets.only(left: 30, right: 30, bottom: 30),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(30),
               color: Colors.white,
               //color: Colors.yellowAccent,
+            ),
+            child: Container(
+              padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  BigText(text: "Pancake Breakfast"),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Wrap(
+                        children: List.generate(
+                            5,
+                            (index) => const Icon(
+                                  Icons.star,
+                                  color: AppColors.mainColor,
+                                  size: 15,
+                                )),
+                      ),
+                      const SizedBox(width: 10),
+                      SmallText(text: "4.5"),
+                      const SizedBox(width: 10),
+                      SmallText(text: "1287"),
+                      const SizedBox(width: 10),
+                      SmallText(text: "comments"),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: const [
+                      IconAndTextWidget(
+                          icon: Icons.circle,
+                          text: "Normal",
+                          iconColor: AppColors.iconColor1),
+                      IconAndTextWidget(
+                          icon: Icons.location_on,
+                          text: "1.7Km",
+                          iconColor: AppColors.mainColor),
+                      IconAndTextWidget(
+                          icon: Icons.access_time_rounded,
+                          text: "32min",
+                          iconColor: AppColors.iconColor2),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
